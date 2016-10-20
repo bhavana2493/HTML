c6dda7bd3efb19cb374b6f74390180fc20916611
bhavana2493 <bhavana4339@gmail.com>
2016-10-20 16:30:03 +0530

    form action prog added

---
+++ form.js
@@ -0,0 +1,20 @@
+
+function display() {
+    var vars = [], hash;
+//taking input from param query......
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        
+        //console.log(hash[i]);
+     vars[hash[0]] = hash[1];
+
+    }
+    //console.log(vars['sname']);
+    return vars;
+}
+//displaying student information.....
+var display1 = display();
+document.myform.sn.value =  display1['sname'];
+document.myform.rn.value =  display1['roll'];
+document.myform.ag.value =  display1['age'];
